f002ef4bfe01d40f40b3f4a156ab84c60393dc48
leandromelolm <leandrosiqueira.pe@gmail.com>
2021-12-16 00:37:21 -0300

    Restrição de conteúdo: usuário só faz um POST da avaliação de estabelecimento com token recebido na autenticação de login  - Só envia passando seu proprio id

--- src/main/java/br/com/sensorsus/sensorsus/services/AvaliacaoEstabelecimentoService.java
+++ src/main/java/br/com/sensorsus/sensorsus/services/AvaliacaoEstabelecimentoService.java
@@ -12,7 +12,10 @@ import br.com.sensorsus.sensorsus.dto.AvaliacaoEstabelecimentoNewDTO;
 import br.com.sensorsus.sensorsus.model.AvaliacaoEstabelecimento;
 import br.com.sensorsus.sensorsus.model.Estabelecimento;
 import br.com.sensorsus.sensorsus.model.Usuario;
+import br.com.sensorsus.sensorsus.model.enums.Perfil;
 import br.com.sensorsus.sensorsus.repositories.AvaliacaoEstabelecimentoRepository;
+import br.com.sensorsus.sensorsus.security.UserSS;
+import br.com.sensorsus.sensorsus.services.exceptions.AuthorizationException;
 import br.com.sensorsus.sensorsus.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -39,9 +42,13 @@ public class AvaliacaoEstabelecimentoService {
 	}
 
 	public AvaliacaoEstabelecimento fromDTO(AvaliacaoEstabelecimentoNewDTO objDto) {
-		Usuario user = new Usuario(objDto.getUsuarioId(), null, null, null, null);
+		UserSS user = UserService.authenticated();
+		if (user==null || !user.hasRole(Perfil.ADMIN) && !objDto.getUsuarioId().equals(user.getId())) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		Usuario usuario = new Usuario(objDto.getUsuarioId(), null, null, null, null);
 		Estabelecimento estab = new Estabelecimento(objDto.getEstabelecimentoId(), null, null, null, null, null);
-		AvaliacaoEstabelecimento avalEstab = new AvaliacaoEstabelecimento(null, objDto.getDataCriacao(), objDto.getDescricao(), objDto.getClassificacao(), user, estab);
+		AvaliacaoEstabelecimento avalEstab = new AvaliacaoEstabelecimento(null, objDto.getDataCriacao(), objDto.getDescricao(), objDto.getClassificacao(), usuario, estab);
 		return avalEstab;
 	}
 
