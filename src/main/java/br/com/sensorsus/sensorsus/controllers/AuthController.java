8bba437836982dd67338ce2d8616defcaea34058
leandromelolm <leandrosiqueira.pe@gmail.com>
2021-12-17 08:36:20 -0300

    Refresh token

---
+++ src/main/java/br/com/sensorsus/sensorsus/controllers/AuthController.java
@@ -0,0 +1,29 @@
+package br.com.sensorsus.sensorsus.controllers;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.sensorsus.sensorsus.security.JWTUtil;
+import br.com.sensorsus.sensorsus.security.UserSS;
+import br.com.sensorsus.sensorsus.services.UserService;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthController {
+	
+	@Autowired
+	private JWTUtil jwtUtil;
+
+	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+		UserSS user = UserService.authenticated();
+		String token = jwtUtil.generateToken(user.getUsername());
+		response.addHeader("Authorization", "Bearer " + token);
+		return ResponseEntity.noContent().build();
+	}
+}
