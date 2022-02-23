9210729147cb196210eedb3523f54e634a5ecd7b
Leandro <leandrosiqueira.pe@gmail.com>
2022-02-22 21:22:18 -0300

    Swagger alteração

--- src/main/java/br/com/sensorsus/sensorsus/config/SwaggerConfig.java
+++ src/main/java/br/com/sensorsus/sensorsus/config/SwaggerConfig.java
@@ -11,16 +11,15 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SpringFoxConfig {
-
-    @Bean
-    public Docket api(){
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
-    }
+public class SwaggerConfig {
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build();
+	}
 }
 // http://localhost:8080/swagger-ui/index.html
 // explorer /sensorsus-openapi
